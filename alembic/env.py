f0a54dc2c02f9c43552b7e2df0dce4740b5d680c
Kazuki Ishijima <kazuki.ishijima@gmail.com>
2026-06-10 00:47:29 +0900

    sqlalchemy connection was fixed.

--- alembic/env.py
+++ alembic/env.py
@@ -7,10 +7,12 @@ from alembic import context
 
 from db.database import Base
 from db import models
+from core.config import settings
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option("sqlalchemy.url", settings.database_url)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
